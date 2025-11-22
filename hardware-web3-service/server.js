c80e1e9378a6dab18c59abbeb9a681ee004a8154
mbcse <mbcse50@gmail.com>
2025-11-22 12:00:00 +0000

    Fix duplicate CORS middleware in Express server

--- hardware-web3-service/server.js
+++ hardware-web3-service/server.js
@@ -57,7 +57,6 @@ function ensureCapturesDirectory() {
 
 const CAPTURES_PATH = ensureCapturesDirectory();
 
-app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
